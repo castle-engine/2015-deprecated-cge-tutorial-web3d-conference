7b99cecdfffcef85af1e2efe5fe4169793ea5165
michaliskambi <michalis.kambi@gmail.com>
2015-06-14 01:17:47 +0200

    Better background part (with castle) on the left. Scale dragon to show left/right move.

--- 2d_game/unit1.pas
+++ 2d_game/unit1.pas
@@ -39,7 +39,7 @@ procedure TForm1.Castle2DControl1Update(Sender: TObject);
   var
     MoveDistance: Single;
   begin
-    MoveDistance := 1000 * Castle2DControl1.Fps.UpdateSecondsPassed;
+    MoveDistance := 500 * Castle2DControl1.Fps.UpdateSecondsPassed;
     if Start < Target then
       Result := Min(Target, Start + MoveDistance) else
       Result := Max(Target, Start - MoveDistance);
@@ -54,6 +54,11 @@ begin
   T[0] := MoveTo(T[0], FlyingTarget[0]);
   T[1] := MoveTo(T[1], FlyingTarget[1]);
   DragonTransform.Translation := T;
+
+  if T[0] < FlyingTarget[0] then
+    DragonTransform.Scale := Vector3Single(-0.2, 0.2, 0.2) else
+  if T[0] > FlyingTarget[0] then
+    DragonTransform.Scale := Vector3Single( 0.2, 0.2, 0.2);
 end;
 
 procedure TForm1.Castle2DControl1Press(Sender: TObject;
@@ -77,11 +82,10 @@ begin
   Castle2DControl1.SceneManager.MainScene := Background;
 
   Castle2DControl1.SceneManager.ProjectionAutoSize := false;
-  Castle2DControl1.SceneManager.ProjectionHeight := 1442;
+  Castle2DControl1.SceneManager.ProjectionHeight := 721;
 
   DragonTransform := T3DTransform.Create(Application);
   DragonTransform.Scale := Vector3Single(0.2, 0.2, 0.2);
-  DragonTransform.Translation := Vector3Single(0, 700, 0);
   Castle2DControl1.SceneManager.Items.Add(DragonTransform);
 
   Dragon := T2DScene.Create(Application);
